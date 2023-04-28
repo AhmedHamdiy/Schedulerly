d4ec1bd24a39b359c4940f36ddbdd99eed52331a
Ahmed_Hmdy <111378492+ahmada28@users.noreply.github.com>
2023-04-28 02:08:22 +0200

    Update Processor.h

--- DSA Project/System Classes/Processors/Processor.h
+++ DSA Project/System Classes/Processors/Processor.h
@@ -1,8 +1,8 @@
 #pragma once
 #include<iostream>
 #include"../Process.h"
-//#include"Scheduler.h"
 using namespace std;
+enum PState { BUSY, IDLE };
 class Processor
 {
 protected:
@@ -11,15 +11,17 @@ protected:
 private:
 	Process* RUN;
 	int busyTime;
-
+	PState state;
 public:
 
 	Processor(); //Constructor
 	virtual void ScheduleAlgo() = 0; //For PHASE2 deteremine how to decide the RUN process
-	void setRUN(Process* p);
+	bool setRUN(Process* p);
 	int getID();
+	void FlipState();
 	bool isIdle(); // returns true if there isn't RUN process
 	Process* GetRunProcess();
+	bool removeProcess();
 	virtual void AddProcess(Process* p) = 0;
 	void setBusytime(int T);
 	int getBusytime();
@@ -30,11 +32,10 @@ public:
 
 	// Overload the comparsion operators by busytime to determine the shortest and longest queue
 	bool operator < (Processor* p1);
-	bool operator >(Processor* p1);
+	bool operator > (Processor* p1);
 	// Overload the << operator as a friend function
 	friend ostream& operator<<(std::ostream& os, const Processor& p);
 	
 	virtual bool RDYtoRUN() = 0;//FOR PHASE 1 ONLY(alternative for scheduelalgo.)//
-	virtual Process* KillRand(int ID);
-
+	
 };
