8782faf954e72608e18711703ed1085a93c2d086
Ahmed_Hmdy <111378492+ahmada28@users.noreply.github.com>
2023-05-06 04:16:45 +0300

    Add files via upload

--- DSA Project/System Classes/Processors/Processor.cpp
+++ DSA Project/System Classes/Processors/Processor.cpp
@@ -1,72 +1,27 @@
 #include"Processor.h"
-
-Processor::Processor() {
+#include "../Scheduler.h"
+Processor::Processor(int Id, Scheduler* sc)
+{
+	ID = Id;
+	MYSch = sc;
 	RUN = nullptr;
 	state = IDLE;
+	StopTime = 0;
 	FinishTime = 0;
 	BusyTime = 0;
 }
-bool Processor::setRUN(Process* p) {
-	if (state == IDLE)
-	{
-		RUN = p;
-		return true;
-	}
-	return false;
-}
-int Processor::getBusytime() {
-	return BusyTime;
-}
-void Processor::Inc_Finishtime(int T)
-{
-	FinishTime += T;
-}
-void Processor::Dec_Finishtime(int T)
-{
-	FinishTime -=T;
-}
 
-void Processor::Inc_BusyTime()
-{
-	if(!isIdle())
-	BusyTime++;
-}
 
-bool Processor::FinishRUN()
-{
-	if (RUN)
-	{
-		if (RUN->getRemainingCT() == 0) 
-		{
-			return true;
-		}
-	}
-	return false;
-}
-void Processor::Dec_RUNCT()
-{
-	if (RUN)
-		RUN->decrementCT();
-}
+//-------------------------------------( Getters )------------------------------------------------//
 
-int Processor::get_Finishtime() {
+int Processor::get_Finishtime()
+{
 	return FinishTime;
 }
 
-ostream& operator<<(std::ostream& os, const Processor& p) {
-	os << "(P" << p.ID << ")";
-	return os;
-}
-
-int Processor::getID() {
-	return ID;
-}
-void Processor::FlipState()
+PState Processor::getState()
 {
-	if (state = BUSY)
-		state = IDLE;
-	else
-		state = BUSY;
+	return state;
 }
 
 bool Processor::isIdle()
@@ -79,12 +34,96 @@ Process* Processor::GetRunProcess()
 	return RUN;
 }
 
+
+
+//-------------------------------------( Setters )------------------------------------------------//
+
+bool Processor::setRUN(Process* p)
+{
+	if (state == IDLE)
+	{
+		RUN = p;
+		return true;
+	}
+	return false;
+}
+void Processor::Inc_Finishtime(int T)
+{
+	FinishTime += T;
+}
+void Processor::Dec_Finishtime(int T)
+{
+	FinishTime -= T;
+}
+
+void Processor::Dec_RUNCT()
+{
+	if (RUN)
+		RUN->decrementCT();
+}
+void Processor::Inc_BusyTime()
+{
+	if(!isIdle())
+	BusyTime++;
+}
+void Processor::UpdateState(PState st)
+{
+	state = st;
+}
+
+
+
+//-------------------------------------( Statistics )------------------------------------------------//
+
+int Processor::getBusytime()
+{
+	return BusyTime;
+}
+
 double Processor::processorLoad(int totalTRT)
 {
-	return (BusyTime/(float)totalTRT)*100;
+	return (BusyTime / (float)totalTRT) * 100;
 }
 
 double Processor::processorUtilization(int timeStep)
 {
-	return (BusyTime/(float)timeStep)*100;
+	return (BusyTime / (float)timeStep) * 100;
 }
+
+
+
+//-------------------------------------( Scheduling )------------------------------------------------//
+
+bool Processor::FinishRUN()
+{
+	if (RUN)
+	{
+		if (RUN->getRemainingCT() == 0)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+void Processor::IO_Req()
+{
+	Pair<int, int> temp;
+	if (GetRunProcess() && GetRunProcess()->GetIO(temp))
+	{
+		if (temp.first == (GetRunProcess()->getCT() - GetRunProcess()->getRemainingCT()))
+		{
+			MYSch->RUNtoBLK(GetRunProcess());
+			setRUN(nullptr);
+		}
+	}
+}
+
+
+//-------------------------------------( Printing )------------------------------------------------//
+
+ostream& operator<<(std::ostream& os, const Processor& p) {
+	os << "(P" << p.ID << ")";
+	return os;
+}
+
--- DSA Project/System Classes/Processors/SJF_Processor.cpp
+++ DSA Project/System Classes/Processors/SJF_Processor.cpp
@@ -1,21 +1,52 @@
 #include "SJF_Processor.h"
+#include"../Scheduler.h"
 
-SJF_Processor::SJF_Processor(int Id)
-{
-	ID = Id; // Assign unique ID 
-}
 
-void SJF_Processor::ScheduleAlgo()
+SJF_Processor::SJF_Processor(int Id,Scheduler* sc):Processor(Id,sc)
+{}
+
+					//-------------------------------------( Scheduling )------------------------------------------------//
+
+int SJF_Processor::OverHeat(Processor* Shortest, int TimeStep, int TStop)
 {
-	/*Process* currentProcess = RDY.peekFront();
-	setRUN(currentProcess);
-	RDY.dequeue();
-	Excuete();*/
+	int Travelled_Proces(0);
+	int randNum = rand() % 100;
+	if (TimeStep - StopTime >= TStop)
+	{
+		if (getState() == BUSY)
+			UpdateState(BUSY);
+		if (getState() == IDLE)
+			UpdateState(IDLE);
+	}
+	else
+		if (randNum < 25)
+		{
+			if (!isIdle())
+			{
+				Process* Rn = GetRunProcess();
+				Shortest->AddProcess(Rn);
+				setRUN(nullptr);
+				Travelled_Proces++;
+			}
+			if (!isRDYempty())
+			{
+				for (int i = 0; i < RDY.getCount(); i++)
+				{
+					Process* p = RDY.Peek();
+					RDY.dequeue();
+					Shortest->AddProcess(p);
+					Dec_Finishtime(p->getRemainingCT());
+					Travelled_Proces++;
+				}
+			}
+			StopTime = TimeStep;
+			UpdateState(STOP);
+		}
+	return Travelled_Proces;
 }
 
 Process* SJF_Processor::remove_Top()
 {
-
 	Process* p = nullptr;
 	if (!RDY.isEmpty())
 	{
@@ -32,18 +63,40 @@ void SJF_Processor::AddProcess(Process* p)
 	Inc_Finishtime(p->getRemainingCT());
 }
 
-bool SJF_Processor::Excuete()
+bool SJF_Processor::isRDYempty()
 {
-	//if it returns true this means the process has ended and will be moved to the TRM list?? 
-	Process* CurProcess = GetRunProcess();
-	if (CurProcess == nullptr)
-		return false;
-	//else if -->>> requires I/O return false
-	else {
-		CurProcess->setCT(0);
-		return true;
+	return RDY.isEmpty();
+}
+
+void SJF_Processor::ScheduleAlgo(int t)
+{
+	if (FinishRUN()) //The  Run Prcocess has Finished
+	{
+		MYSch->MoveToTRM(GetRunProcess());
+		setRUN(nullptr);
+	}
+	IO_Req();
+	if (!isIdle())
+	{
+		Inc_BusyTime();	
+		Dec_RUNCT();
+	}
+	else{
+
+		if (isRDYempty())
+			return;
+
+		//Choose The Next Run Process 
+		Process* RDYprocess = RDY.Peek();
+		RDY.dequeue();
+		RDYprocess->updateState(RUNNING);
+		Dec_Finishtime(RDYprocess->getRemainingCT());
+		setRUN(RDYprocess);
+		RDYprocess->setstart(t);
 	}
 }
+ 
+			//-----------------------------------------( Printing )------------------------------------------------//
 
 void SJF_Processor::printRDY()
 {
@@ -52,26 +105,5 @@ void SJF_Processor::printRDY()
 
 void SJF_Processor::printInfo()
 {
-	//cout << "processor " << ID << " [SJF]: " << RDY.getsize() << " ";
 	cout << " [SJF ]: " << RDY.getCount() << " ";
 }
-
-bool SJF_Processor::isRDYempty()
-{
-	return RDY.isEmpty();
-}
-
-bool SJF_Processor::RDYtoRUN(int t, Scheduler* scptr)
-{
-	if (isRDYempty() || !isIdle())
-		return false;
-	Process* RDYprocess = RDY.Peek();
-	RDY.dequeue();
-	RDYprocess->updateState(RUNNING);
-	Dec_Finishtime(RDYprocess->getRemainingCT());
-	setRUN(RDYprocess);
-	RDYprocess->setstart(t);
-	return 1;
-}
-
-
